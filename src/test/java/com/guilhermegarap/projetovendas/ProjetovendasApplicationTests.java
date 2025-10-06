cca521e05f976b514f52592de791781af6456bf2
GuilhermeGarap <guilhermegarap@gmail.com>
2025-10-06 11:59:19 -0300

    Projeto Criado

---
+++ src/test/java/com/guilhermegarap/projetovendas/ProjetovendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermegarap.projetovendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetovendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
